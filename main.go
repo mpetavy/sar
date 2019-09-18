532ce48448290d59b89dafe4a67afd5a5d5e45ad
mpetavy <mpetavy>
2019-09-18 12:46:21 +0200

    os.ModePerm -> DirFileMode, FileFileMode

--- main.go
+++ main.go
@@ -190,7 +190,7 @@ func processFile(filename string) error {
 				return err
 			}
 
-			err = ioutil.WriteFile(filename, []byte(output), os.ModePerm)
+			err = ioutil.WriteFile(filename, []byte(output), common.FileFileMode)
 			if err != nil {
 				return err
 			}
